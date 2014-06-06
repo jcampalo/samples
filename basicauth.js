a62708921e9c16bbec6ffd491e59d947e6441d69
Javier <javier.campa-lopez@capgemini.com>
2014-06-06 13:02:50 +0200

    Update basicauth.js
    Basic Authentication in Node with connect

--- basicauth.js
+++ basicauth.js
@@ -6,7 +6,7 @@ connect.basicAuth(function (user, pass, fn) {
   process.stdout.write("Allow user \033[96m" + user + "\033[39m "
     + " with pass \033[90m" + pass + "\033[39m ? [y/n]: ");
   process.stdin.once("data", function (data) {
-    if (data[0] == "y")
+    if (data[0] === "y")
       fn(null, { username: user })
    else
       fn(new Error("Unauthorized"));
