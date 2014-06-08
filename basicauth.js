cf00d4b629d991090da108be0421822b1c6ea492
jcampalo <javier.campa-lopez@capgemini.com>
2014-06-08 22:39:51 +0200

    Update basicauth.js

--- basicauth.js
+++ basicauth.js
@@ -3,17 +3,17 @@ process.stdin.resume();
 process.stdin.setEncoding("ascii");
 var server = connect(
 connect.basicAuth(function (user, pass, fn) {
-  process.stdout.write("Allow user \033[96m" + user + "\033[39m "
-    + " with pass \033[90m" + pass + "\033[39m ? [y/n]: ");
-  process.stdin.once("data", function (data) {
-    if (data[0] === "y")
-      fn(null, { username: user })
-   else
-      fn(new Error("Unauthorized"));
-  });
+    process.stdout.write("Allow user \033[96m" + user + "\033[39m "
+        + " with pass \033[90m" + pass + "\033[39m ? [y/n]: ");
+    process.stdin.once("data", function (data) {
+        if (data[0] === "y")
+            fn(null, { username: user })
+        else
+            fn(new Error("Unauthorized"));
+    });
 }),
 function (req, res) {
-  res.writeHead(200);
-  res.end("Welcome to the protected area, " + req.remoteUser.username);
+    res.writeHead(200);
+    res.end("Welcome to the protected area, " + req.remoteUser.username);
 });
 server.listen(3000);
